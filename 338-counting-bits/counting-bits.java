61f9b9121fcb5a0faa5109ef0adee7d089683ff9
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-09-01 09:11:35 +0530

    Time: 6 ms (25.19%) | Memory: 46.4 MB (66.16%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int ans[] = new int[n + 1];
+        for(int i=0; i<=n; i++) {
+            int sum = 0;
+            int num = i;
+            while(num != 0) {
+                sum += num % 2;
+                num = num / 2;
+            }
+            ans[i] = sum;
+        }
+
+        return ans;
+    }
+}
