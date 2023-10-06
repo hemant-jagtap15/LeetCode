908e886040973feda91b3432bad368f983743821
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-10-06 07:18:46 +0530

    Time: 1 ms (61.46%) | Memory: 39.1 MB (63.93%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -4,19 +4,15 @@ class Solution {
             return n - 1;
         }
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return ways(n, dp);
-    }
+        for(int i = 0; i <= 3; i++) dp[i] = i;
 
-    public int ways(int num, int dp[]) {
-        if(num <= 3) {
-            return num;
-        }
-        if(dp[num] != -1) return dp[num];
-        int ans = num;
-        for(int i = 2; i < num; i++) {
-            ans = Math.max(ans, i * ways(num - i, dp));
-        }
-        return dp[num] = ans;
+        for(int num = 4; num <= n; num++) {
+            int ans = num;
+            for(int i = 2; i < num; i++) {
+                ans = Math.max(ans, i * dp[num - i]);
+            }
+            dp[num] = ans;
+        } 
+        return dp[n];
     }
 }
