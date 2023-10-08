bcba3949728466db15003e4eccf0418731d77b35
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-10-08 16:20:41 +0530

    Time: 9 ms (94.44%) | Memory: 43.2 MB (37.50%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -3,16 +3,17 @@ class Solution {
         int n = nums1.length;
         int m = nums2.length;
         int dp[][] = new int[n+1][m+1];
-        for(int arr[] : dp) Arrays.fill(arr, -1);
-        return ways(n, m, nums1, nums2, dp);
-    }
-    public int ways(int i, int j, int nums1[], int nums2[], int dp[][]) {
-        if(i == 0 || j == 0) return Integer.MIN_VALUE;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int dot = nums1[i-1] * nums2[j-1] + Math.max(ways(i-1, j-1, nums1, nums2, dp), 0);
-        int notDot = Math.max(ways(i, j-1, nums1, nums2, dp), ways(i-1, j, nums1, nums2, dp));
-        
-        return dp[i][j] = Math.max(dot, notDot);
+        for(int i = 0; i <= n; i++) dp[i][0] = Integer.MIN_VALUE;
+        for(int i = 0; i <= m; i++) dp[0][i] = Integer.MIN_VALUE;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                int dot = nums1[i-1] * nums2[j-1] + Math.max(dp[i-1][j-1], 0);
+                int notDot = Math.max(dp[i][j-1], dp[i-1][j]);
+                
+                dp[i][j] = Math.max(dot, notDot);
+            }
+        }
+        return dp[n][m];
     }
 }
