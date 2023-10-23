5774a7eaaa929393460d0068aade73b2110cf1a3
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-10-23 12:47:49 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (34.17%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
-        int x = (int)(Math.log(n) / Math.log(4));
-        return Math.pow(4, x) == n;
+        //number is power of 4, than it is power of 2 as well (n & (n-1))
+        //with this number - 1 is also divisible by 3
+        if((n & (n-1)) == 0 && (n-1) % 3 == 0) return true;
+        return false;
     }
 }
