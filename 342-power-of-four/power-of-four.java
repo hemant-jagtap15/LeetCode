b0fec05110ccf115375027f799fc3625a1487156
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-10-23 12:42:16 +0530

    Time: 1 ms (76.31%) | Memory: 39.2 MB (83.82%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0) return false;
+        int x = (int)(Math.log(n) / Math.log(4));
+        return Math.pow(4, x) == n;
+    }
+}
