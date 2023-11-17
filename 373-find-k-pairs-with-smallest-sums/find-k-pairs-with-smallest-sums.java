351032e513e485bc5194dc1d4f2f92501f0030e0
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-11-17 15:46:11 +0530

    Time: 61 ms (26.56%) | Memory: 60.2 MB (17.50%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,59 +1,57 @@
+import java.util.*;
+
 class Solution {
-    // class Sums implements Comparable<Sums> {
-    //     ArrayList<Integer> arr;
-    //     int sum;
-
-    //     Sums(ArrayList<Integer> arr, int sum) {
-    //         this.arr = arr;
-    //         this.sum = sum;
-    //     }
-
-    //     public int compareTo(Sums s2) {
-    //         return this.sum - s2.sum;
-    //     }
-    // }
-
     class Sums {
-        ArrayList<Integer> arr;
+        int x;
+        int y;
         int sum;
 
-        Sums(ArrayList<Integer> arr, int sum) {
-            this.arr = arr;
+        Sums(int x, int y, int sum) {
+            this.x = x;
+            this.y = y;
             this.sum = sum;
         }
     }
 
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        PriorityQueue<Sums> pq = new PriorityQueue<>((x, y) -> Integer.compare(y.sum, x.sum));
+        PriorityQueue<Sums> pq = new PriorityQueue<>((x, y) -> Integer.compare(x.sum, y.sum));
+        pq.add(new Sums(0, 0, nums1[0] + nums2[0]));
 
-        for(int i = 0; i < nums1.length; i++) {
-            for(int j = 0; j < nums2.length; j++) {
-                ArrayList<Integer> arr = new ArrayList<>();
-                int sum = nums1[i] + nums2[j];
-                if(pq.size() < k) {
-                    arr.add(nums1[i]);
-                    arr.add(nums2[j]);
-                    pq.add(new Sums(arr, sum));
-                } else if(pq.peek().sum > sum) {
-                    pq.remove();
-                    arr.add(nums1[i]);
-                    arr.add(nums2[j]);
-                    pq.add(new Sums(arr, sum));
-                } else {
-                    break;
+        HashSet<ArrayList<Integer>> set = new HashSet<>(); // visited
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(0);
+        temp.add(0);
+        set.add(temp);
+
+        while (!pq.isEmpty() && k > 0) {
+            Sums it = pq.poll();
+            int i = it.x;
+            int j = it.y;
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(nums1[i]);
+            list.add(nums2[j]);
+            ans.add(list);
+
+            if (j + 1 < nums2.length) {
+                ArrayList<Integer> next = new ArrayList<>();
+                next.add(i);
+                next.add(j + 1);
+                if (!set.contains(next)) {
+                    pq.add(new Sums(i, j + 1, nums1[i] + nums2[j + 1]));
+                    set.add(next);
                 }
             }
-        }
-
-        for(int i = 0; i < k; i++) {
-            if(!pq.isEmpty()) {
-                ans.add(pq.peek().arr);
-                pq.remove();
-            } else {
-                break;
+            if (i + 1 < nums1.length) {
+                ArrayList<Integer> next = new ArrayList<>();
+                next.add(i + 1);
+                next.add(j);
+                if (!set.contains(next)) {
+                    pq.add(new Sums(i + 1, j, nums1[i + 1] + nums2[j]));
+                    set.add(next);
+                }
             }
-            
+            k--;
         }
 
         return ans;
