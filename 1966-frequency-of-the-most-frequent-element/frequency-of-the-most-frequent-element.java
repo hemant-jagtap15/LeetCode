12bf1feb8e49e0dc3104acf89eff2da43d444d88
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-11-18 15:57:40 +0530

    Time: 29 ms (72.60%) | Memory: 57.7 MB (25.09%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -2,44 +2,22 @@ class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
         int n = nums.length;
-        int max = 0;
+        int ans = 0;
+        int left = 0;
+        int right = 0;
+        long curr = 0;
 
-        long prefixSum[] = new long[n];
-        prefixSum[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
+        while(right < n) {
+            int target = nums[right];
+            curr += target;
 
-        int i = 0;
-        while(i < n) {
-            int targetIdx = i;
-            int equals = maxPossible(nums, prefixSum, k, targetIdx);
-            max = Math.max(max, equals);
-            i++;
-        }
-        return max;
-    }
-
-    public int maxPossible(int nums[], long prefixSum[], int k, int targetIdx) {
-        int low = 0;
-        int high = targetIdx - 1;
-        int target = nums[targetIdx];
-        int best = targetIdx;
-
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            long noBtw = targetIdx - mid + 1;
-            long totSum = target * noBtw;
-            long originalSum = prefixSum[targetIdx] - prefixSum[mid] + nums[mid];
-            long operations = totSum - originalSum;
-
-            if(operations > k) {
-                low = mid + 1;
-            } else {
-                best = mid;
-                high = mid - 1;
+            while((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
             }
-        }
-        return targetIdx - best + 1;
+            ans = Math.max(ans, right - left + 1);
+            right++;
+         }
+        return ans;
     }
 }
