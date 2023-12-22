7772d135df42b5197b982603a4ae044ccccb8048
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2023-12-22 10:04:08 +0530

    Time: 1 ms (97.78%) | Memory: 41.5 MB (32.89%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -2,28 +2,20 @@ class Solution {
     public int maxScore(String s) {
         int n = s.length();
         int ans = 0;
+        int rightOne = 0;
+        for(int i = 0; i < n; i++) {
+            if(s.charAt(i) == '1') rightOne++;
+        }
+        int leftZero = 0;
+
         for(int i = 0; i < n-1; i++) {
-            int left = leftZero(i, s);
-            int right = rightOne(i+1, s);
-            ans = Math.max(left + right, ans);
+            if(s.charAt(i) == '1') {
+                rightOne--;
+            } else {
+                leftZero++;
+            }
+            ans = Math.max(ans, rightOne + leftZero);
         }
         return ans;
     }
-
-    public int leftZero(int i, String s) {
-        int zeroCnt = 0;
-        for(int j = 0; j <= i; j++) {
-            if(s.charAt(j) == '0') zeroCnt++;
-        }
-        return zeroCnt;
-    }
-
-    public int rightOne(int i, String s) {
-        int oneCnt = 0;
-        for(int j = i; j < s.length(); j++) {
-            if(s.charAt(j) == '1') oneCnt++;
-        }
-        return oneCnt;
-    }
-
 }
