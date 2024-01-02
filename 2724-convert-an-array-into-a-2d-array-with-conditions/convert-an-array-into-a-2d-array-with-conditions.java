ed8d04a416c147faf7fdff68cc88b3babef9c8e6
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2024-01-02 12:00:13 +0530

    Time: 4 ms (55.08%) | Memory: 45.1 MB (9.75%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,23 +1,16 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int val : nums) {
-            map.put(val, map.getOrDefault(val, 0) + 1);
-        }
+        int n = nums.length;
+        int freq[] = new int[n+1];
 
-        while(!map.isEmpty()) {
-            List<Integer> list = new ArrayList<>();
-            for(Integer key : map.keySet()) {
-                list.add(key);
-                map.put(key, map.get(key) - 1);
+        for(int val : nums) {
+            if(freq[val] >= ans.size()) {
+                ans.add(new ArrayList<>());
             }
-            for(int val : list) {
-                if(map.get(val) == 0) map.remove(val);
-            }
-            
-            ans.add(list);
+
+            ans.get(freq[val]).add(val);
+            freq[val]++;
         }
 
         return ans;
