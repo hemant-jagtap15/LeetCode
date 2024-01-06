7fd87ff00b8b92a300a105e7ea9e7825f3209d6d
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2024-01-06 10:26:18 +0530

    Time: 25 ms (92.07%) | Memory: 55.7 MB (50.58%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -20,27 +20,24 @@ class Solution {
         // for(int i = 0; i < n; i++) {
         //     System.out.println(jobs[i].start + " " + jobs[i].end + " " + jobs[i].pro);
         // }
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-
-        return ways(0, jobs, dp);
-    }
-
-    public int ways(int i, Tuple jobs[], int dp[]) {
-        if(i == jobs.length) return 0;
+        int dp[] = new int[n+1];
+        int max = 0;
         
-        if(dp[i] != -1) return dp[i];
+        for(int i = n-1; i >= 0; i--) {
+            int notTake = dp[i+1];
 
-        int notTake = ways(i+1, jobs, dp);
+            int take = jobs[i].pro;
+            int nextIdx = lowerBound(jobs[i].end, jobs);
+            
+            if (nextIdx != -1) {
+                take += dp[nextIdx];
+            }
 
-        int take = jobs[i].pro;
-        int nextIdx = lowerBound(jobs[i].end, jobs);
-        
-        if (nextIdx != -1) {
-            take += ways(nextIdx, jobs, dp);
+            dp[i] = Math.max(take, notTake);
+            max = Math.max(dp[i], max);
         }
 
-        return dp[i] = Math.max(take, notTake);
+        return max;
     }
 
     public int lowerBound(int k, Tuple jobs[]) {
