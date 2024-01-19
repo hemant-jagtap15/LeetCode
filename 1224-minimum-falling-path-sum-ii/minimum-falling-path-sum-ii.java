05977e42370602a8818c98d3e31adfba9adb4455
hemant-jagtap15 <115880007+hemant-jagtap15@users.noreply.github.com>
2024-01-19 12:24:32 +0530

    Time: 35 ms (57.85%) | Memory: 51.6 MB (23.77%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,30 +1,32 @@
 class Solution {
-    public int ways(int i, int j, int grid[][], int dp[][]) {
-        if(i == 0) return grid[0][j];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int minPath = (int)(1e9);
-        for(int k = 0; k < grid[0].length; k++) {
-            if(k == j) continue;
-            int curr = grid[i][j] + ways(i-1, k, grid, dp);
-            minPath = Math.min(minPath, curr);
-        }
-
-        return dp[i][j] = minPath;
-    }
     public int minFallingPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int dp[][] = new int[n][m];
-        for(int row[] : dp) {
-            Arrays.fill(row, -1);
+        
+        for(int j = 0; j < m; j++) {
+            dp[0][j] = grid[0][j];
         }
 
         int min = (int)(1e9);
-        for(int j = 0; j < m; j++) {
-            min = Math.min(min, ways(n-1, j, grid, dp));
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int minPath = (int)(1e9);
+                for(int k = 0; k < grid[0].length; k++) {
+                    if(k == j) continue;
+                    int curr = grid[i][j] + dp[i-1][k];
+                    minPath = Math.min(minPath, curr);
+                }
+
+                dp[i][j] = minPath;
+            }
         }
+
+        for(int j = 0; j < m; j++) {
+            min = Math.min(min, dp[n-1][j]);
+        }
+        
         return min;
     }
 }
